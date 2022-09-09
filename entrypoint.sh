325b16f9c6a9cc5b0ba06d417231c43a88bf1bfb
Robin Du Admin <robinduadmin@PMB-DEV02.each.com.au>
2022-09-09 13:56:33 +1000

    deployment setting

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+touch ./logs/gunicorn.log
+touch ./logs/gunicorn-access.log
+tail -n 0 -f ./logs/gunicorn*.log &
+
+exec gunicorn power_api.wsgi:application \
+    --name hrar_azure \
+    --bind 0.0.0.0:8000 \
+    --workers 4 \
+    --log-level=info \
+    --limit-request-line 0 \
+    --log-file=./logs/gunicorn.log \
+    --access-logfile=./logs/gunicorn-access.log \
+"$@"
